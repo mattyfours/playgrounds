0449bd143c7de703a1240e6f21e18985a2a7520c
Matthew Fournier <matthew@halfhelix.com>
2026-01-12 17:14:07 -0500

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-import fs from 'fs'
-import path from 'path'
-
-function getLiquidContent() {
-  const playgroundsDir = './playgrounds'
-  const liquidFiles = []
-  
-  function walkDir(dir) {
-    const files = fs.readdirSync(dir)
-    files.forEach(file => {
-      const fullPath = path.join(dir, file)
-      const stat = fs.statSync(fullPath)
-      if (stat.isDirectory()) {
-        walkDir(fullPath)
-      } else if (file.endsWith('.liquid')) {
-        liquidFiles.push(fullPath)
-      }
-    })
-  }
-  
-  walkDir(playgroundsDir)
-  return liquidFiles
-}
-
 export default {
   content: [
-    './core/**/*.{html,js,ts,jsx,tsx}',
-    './playgrounds/**/*.{html,js,ts,jsx,tsx}',
-    ...getLiquidContent(),
+    '**/*.{html,js,ts,jsx,tsx,liquid}',
   ],
   theme: {
     extend: {},
